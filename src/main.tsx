649543830891ab52cfa48f921d34153f0b6c0b3f
Júlia Eduarda Costa dos Anjos <julia.anjos@maxmilhas.com.br>
2023-08-06 00:52:22 -0300

    add router using react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/index.js';
 import { GlobalStyles } from './styles/global-styles.js';
 import { MyThemeProvider } from './styles/MyThemeProvider.js';
-import { App } from './template/App/index.js';
+import App from './template/App/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MyThemeProvider>
-      <App />
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={App} />
+        </Routes>
+      </BrowserRouter>
       <GlobalStyles />
     </MyThemeProvider>
   </React.StrictMode>
